0b425c7be05a24cf197a07b82e3fae93b6dcbd29
wamei <wamei.cho@gmail.com>
2020-08-04 16:23:22 +0900

    fix build environment

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,51 +3,45 @@ import gulpLoadPlugins from 'gulp-load-plugins';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
 import del from 'del';
-import run from 'run-sequence';
 
 const $ = gulpLoadPlugins();
 
 gulp.task('build-js', () => {
-    return gulp.src('src/**/*.js')
-        .pipe($.babel())
-        .pipe(gulp.dest('built'));
+  return gulp.src('src/**/*.js')
+    .pipe($.babel())
+    .pipe(gulp.dest('built'));
 });
 
 gulp.task('browserify', () => {
-    return browserify({
-        entries: ['built/app.js']
-    })
-        .bundle()
-        .pipe(source('boxrtc.js'))
-        .pipe(gulp.dest('./'));
+  return browserify({
+    entries: ['built/app.js']
+  })
+    .bundle()
+    .pipe(source('boxrtc.js'))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('clean-built', () => {
-    return del(['built/*']);
+  return del(['built/*']);
 });
 
 gulp.task('webserver', () => {
-    return gulp.src('./')
-        .pipe($.webserver({
-            host: '0.0.0.0',
-            livereload: {
-                enable: true,
-                filter: function(fileName) {
-                    switch(fileName.substr(__dirname.length + 1)) {
-                    case 'index.html':
-                    case 'boxrtc.js':
-                        return true;
-                    }
-                    return false;
-                }
-            },
-            open: true
-        }));
+  return gulp.src('./')
+    .pipe($.webserver({
+      host: '0.0.0.0',
+      livereload: {
+        enable: true,
+        filter: function(fileName) {
+          switch (fileName.substr(__dirname.length + 1)) {
+            case 'index.html':
+            case 'boxrtc.js':
+              return true;
+          }
+          return false;
+        }
+      },
+      open: true
+    }));
 });
 
-gulp.task('build', (callback) => {
-    return run('clean-built',
-               'build-js',
-               'browserify',
-               callback);
-});
+gulp.task('build', gulp.series('clean-built', 'build-js', 'browserify'));
